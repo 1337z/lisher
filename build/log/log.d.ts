b72ac235298997cac2edd4249eaf322217a03b85
MarvinJWendt <github@marvinjwendt.com>
2018-12-23 03:18:30 +0100

    Remove declaration files This module shouldn't be required anyways

--- build/log/log.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const log: {
-    (message?: any, ...optionalParams: any[]): void;
-    (message?: any, ...optionalParams: any[]): void;
-};
-export declare const info: (message: string) => void;
-export declare const debugInfo: (message: any) => void;
--- build/utils/exec.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-import { execSync } from "child_process";
-export declare const execRaw: typeof execSync;
-export declare const exec: (command: string) => void;
