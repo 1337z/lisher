85b4e3440ca762d990610addcb8d2090b083ab94
MarvinJWendt <github@marvinjwendt.com>
2018-12-23 18:33:28 +0100

    Add version command

--- bin/cli-lisher.js
+++ bin/cli-lisher.js
@@ -4,11 +4,17 @@ const lisher = require("../build/lisher")
 const updateNotifier = require("update-notifier")
 const pkg = require("../package.json")
 
-const argv = require("yargs").option("debug", {
+const argv = require("yargs")
+.option("debug", {
   describe: "Enable debugging",
   alias: "d",
   default: false
-}).argv
+}).option("version", {
+  describe: "Show the installed version of lisher",
+  alias: "v",
+  default: false
+})
+.argv
 
 updateNotifier({ pkg }).notify()
 
