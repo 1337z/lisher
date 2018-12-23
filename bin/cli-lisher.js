82d6713e7855259abedb39183bc6cf3d902b2928
MarvinJWendt <github@marvinjwendt.com>
2018-12-23 03:28:35 +0100

    Added update notification

--- bin/cli-lisher.js
+++ bin/cli-lisher.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
 const lisher = require("../build/lisher")
+const updateNotifier = require("update-notifier")
+const pkg = require("../package.json")
 
 const argv = require("yargs").option("debug", {
   describe: "Enable debugging",
   alias: "d",
   default: false
 }).argv
+
+updateNotifier({ pkg }).notify()
 
 lisher.run(argv)
