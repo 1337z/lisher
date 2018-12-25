6608c4c9c9e01a5353ea0bea0c9813c7cc2887c0
MarvinJWendt <github@marvinjwendt.com>
2018-12-25 22:12:46 +0100

    Add cli --version test

---
+++ test/cli.js
@@ -0,0 +1,11 @@
+import test from "ava"
+import * as lisher from "../build/lisher"
+import { execSync } from "child_process"
+import { readFileSync } from "fs";
+const mockIo = require("mock-stdio")
+
+test("Argument: --version", t => {
+  let result = execSync("node ./bin/cli-lisher --version").toString()
+
+  t.is(result, JSON.parse(readFileSync("package.json")).version + "\n")
+})
