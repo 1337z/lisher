f4560cbca9d905c4e9ea75db27283d0cb830a01f
MarvinJWendt <github@marvinjwendt.com>
2018-12-30 23:34:21 +0100

    Refactor

--- test/test-system.js
+++ test/test-system.js
@@ -1,12 +1,10 @@
 import test from "ava"
-import * as lisher from "../build/lisher"
+
 const mockIo = require("mock-stdio")
 
-test("Test tests (if this test doensn't pass: The testing system might be broken, which is bad)", t => {
+test("Test tests (On fail: The testing system might be broken, which is bad)", t => {
   mockIo.start()
-
   console.log("Test")
-
   let result = mockIo.end()
 
   t.is(result.stdout, "Test\n")
