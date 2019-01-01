e0f154870bcb49fd5a0c2b54868a53ee6619dcc3
MarvinJWendt <github@marvinjwendt.com>
2019-01-01 23:03:42 +0100

    Sort lists in acending order

--- test/jsonHelper.js
+++ test/jsonHelper.js
@@ -1,6 +1,6 @@
-import test from "ava"
 import * as fs from "fs"
 import * as jsonHelper from "../build/utils/jsonHelper"
+import test from "ava"
 
 test("Get JSON from file", t => {
   t.deepEqual(JSON.parse(fs.readFileSync("./package.json").toString()), jsonHelper.fromFile("./package.json"))
